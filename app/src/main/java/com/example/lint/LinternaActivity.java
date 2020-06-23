9484425d06df0e78248e5e992b153b9bc83dec9f
mauro-nieva <mauro_ramones_86@hotmail.com>
2020-06-23 03:05:16 -0300

    Comprobacion de conexion a internet. Cambio de modo en automatico.

--- app/src/main/java/com/example/lint/LinternaActivity.java
+++ app/src/main/java/com/example/lint/LinternaActivity.java
@@ -2,6 +2,7 @@ package com.example.lint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.hardware.Camera;
@@ -9,17 +10,26 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class LinternaActivity extends AppCompatActivity {
 
     private ImageView boton;
+    private ImageView imgInternet;
+    private ImageView imgModo;
+    private boolean isAuto=false;
 
     private TextView txtLuminosidad,txtProximidad;
 
@@ -37,12 +47,19 @@ public class LinternaActivity extends AppCompatActivity {
     boolean enBolsillo=false;
     Tarea_Proximidad AsyncTask_Proximidad;
 
+    private Handler handlerInternet;
+    private Timer timerInternet;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linterna);
 
         boton=(ImageView) findViewById(R.id.imgLinterna);
+        imgInternet=(ImageView) findViewById(R.id.imgInternet);
+        imgModo=(ImageView) findViewById(R.id.imgModo);
+
+        imgModo.setOnClickListener(HandlerCmdModo);
 
         //mantener pantalla encendida
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
@@ -80,7 +97,30 @@ public class LinternaActivity extends AppCompatActivity {
             parametros=camara.getParameters();
             isFlash=true;
         }
+
+        handlerInternet = new Handler();
+        timerInternet = new Timer();
+
     }
+
+    View.OnClickListener HandlerCmdModo=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(isAuto)
+            {
+                imgModo.setImageResource(R.drawable.manual);
+                cancelarAuto();
+                isAuto=false;
+            }
+            else
+            {
+                imgModo.setImageResource(R.drawable.auto);
+                ejecutarAuto();
+                isAuto=true;
+            }
+
+        }
+    };
 
     public void setBolsillo (Boolean b) {
         enBolsillo=b;
@@ -90,6 +130,28 @@ public class LinternaActivity extends AppCompatActivity {
         return enBolsillo;
     }
 
+    public TimerTask timerTaskInternet = new TimerTask() {
+        @Override
+        public void run() {
+            handlerInternet.post(new Runnable() {
+                public void run() {
+                    try {
+                        Tarea_Internet AsyncTask_Internet=new Tarea_Internet();
+                        // PerformBackgroundTask this class is the class that extends AsynchTask
+                        AsyncTask_Internet.execute();
+                    } catch (Exception e) {
+                        // TODO Auto-generated catch block
+                    }
+                }
+            });
+        }
+    };
+
+    public void muestraMensaje(String mensaje)
+    {
+
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
+    }
 
 
     public void enciendeFlash()
@@ -117,17 +179,38 @@ public class LinternaActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        AsyncTask_Luminosidad.execute(sensorManager,lightSensor);
-        AsyncTask_Proximidad.execute(sm_Proximidad,s_Proximidad);
+        if(isAuto) {
+            ejecutarAuto();
+        }
+        timerInternet.schedule(timerTaskInternet, 0, 5000);
 
+    }
+
+    protected void ejecutarAuto()
+    {
+        AsyncTask_Proximidad=new Tarea_Proximidad();
+        AsyncTask_Luminosidad=new Tarea_Luminosidad();
+        AsyncTask_Luminosidad.execute(sensorManager, lightSensor);
+        AsyncTask_Proximidad.execute(sm_Proximidad, s_Proximidad);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        if(isAuto) {
+            cancelarAuto();
+            apagaFlash();
+        }
+
+        timerInternet.cancel();
+    }
+
+    protected  void cancelarAuto()
+    {
         AsyncTask_Luminosidad.cancel(true);
         AsyncTask_Proximidad.cancel(true);
-        apagaFlash();
+
+        //apagaFlash();
     }
 
 
@@ -262,6 +345,42 @@ public class LinternaActivity extends AppCompatActivity {
             }
         };
     }
+
+    class Tarea_Internet extends AsyncTask {
+        @Override
+        protected Object doInBackground(Object[] objects) {
+
+            String mensaje;
+
+            ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+            if (networkInfo != null && networkInfo.isConnected()) {
+                mensaje="SI";
+            } else {
+                mensaje="NO";
+            }
+
+            return mensaje;
+        }
+
+        @Override
+        protected void onProgressUpdate(Object[] values) {
+            super.onProgressUpdate(values);
+
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+
+            if(o.equals("SI"))
+                imgInternet.setImageResource(R.drawable.internet_on);
+            else
+                imgInternet.setImageResource(R.drawable.internet_off);
+        }
+    }
+
 }
 
 
